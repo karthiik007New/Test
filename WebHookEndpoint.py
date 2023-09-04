6df4f8c38ec79e311cca4f19edb7d0de2fecbb86
karthiik007New <142682186+karthiik007New@users.noreply.github.com>
2023-09-04 12:08:57 +0800

    sample web hook test

---
+++ WebHookEndpoint.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 09:57:49 2023
+
+@author: karthikj
+"""
+
+from flask import Flask, request, jsonify
+
+from flask_cors import CORS  # Import the CORS class
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def WebHook():
+    data = request.get_json()
+    
+    print(data)
+
+    return jsonify(True)
+
+
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(debug=True,port=9000)
